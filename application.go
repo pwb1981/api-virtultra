e45e4d6cb839f89b727751c2a614e975c984636a
Paul B <pwb1981@gmail.com>
2020-05-25 10:35:13 +0100

    tidy

--- application.go
+++ application.go
@@ -1,10 +1,51 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
 )
+
+func routeHandler(w http.ResponseWriter, r *http.Request, route string) {
+	var (
+		p []byte
+		err error)
+
+	if route == "all" {
+		p, err = getAllRoutes()
+	} else {
+		p, err = getRoute(route)
+	}
+	
+	if err != nil {
+		fmt.Fprintf(w, "%s", "ERROR")
+		return
+	}
+	
+	fmt.Fprintf(w, "%s", p)
+}
+
+func getRoute(routeId string) ([]byte, error) {
+	filename := "routes/" + routeId + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func getAllRoutes() ([]byte, error) {
+	filename := "routes/all.txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
 
 var validPath = regexp.MustCompile("^/api/(route|SOMEOTHERROUTE)/([a-zA-Z0-9]+)$")
 
